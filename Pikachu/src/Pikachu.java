6674cf30a1d67a3971138e0dd602cb29491093a7
Joseph <josephartura@gmail.com>
2018-09-06 09:15:04 -0700

    It's a matrix of Pikachus!!!!!

--- Pikachu/src/Pikachu.java
+++ Pikachu/src/Pikachu.java
@@ -1,6 +1,28 @@
 public class Pikachu {
     public static void main(String[] args){
         String Pikachu = "  (\\__/)\n  (*^.^)\nz(_\")(\")";
-        System.out.println(Pikachu);
+
+        String Pikachu1 = "  (\\__/)";
+        String Pikachu2 = "  (*^.^)";
+        String Pikachu3 = "z(_\")(\")";
+
+        int Pika = 3;
+        int Puka = 3;
+
+        for(int j = 0; j < Puka; j++) {
+            for (int i = 0; i < Pika; i++) {
+                System.out.print(Pikachu1);
+            }
+            System.out.println();
+            for (int i = 0; i < Pika; i++) {
+                System.out.print(Pikachu2);
+            }
+            System.out.println();
+            for (int i = 0; i < Pika; i++) {
+                System.out.print(Pikachu3);
+            }
+            System.out.println();
+        }
+        System.out.println("There have been " + (Pika * Puka) + " Pikachus printed");
     }
 }
