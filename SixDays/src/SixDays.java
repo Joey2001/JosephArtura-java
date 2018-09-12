e31a1420e0691c500c29939d0d9c06297081dc32
Joseph <josephartura@gmail.com>
2018-09-12 09:55:38 -0700

    The first project

--- SixDays/src/SixDays.java
+++ SixDays/src/SixDays.java
@@ -11,71 +11,71 @@
 */
 public class SixDays {
     public static void main(String[] args){
-        System.out.println();
         Verse1();
-        Day1();
         Verse2();
-        Day2();
         Verse3();
-        Day3();
         Verse4();
-        Day4();
         Verse5();
-        Day5();
         Verse6();
-        Day6();
     }
     //This prints the gifts for Day 1
-    public static void Day1(){
+    private static void Day1(){
         System.out.println("a partridge in a pear tree.\n");
     }
     //This prints the gifts for Day 2
-    public static void Day2(){
+    private static void Day2(){
         System.out.println("two turtle doves, and");
         Day1();
     }
     //This prints the gifts for Day 3
-    public static void Day3(){
+    private static void Day3(){
         System.out.println("three French hens,");
         Day2();
     }
     //This prints the gifts for Day 4
-    public static void Day4(){
+    private static void Day4(){
         System.out.println("four calling birds");
         Day3();
     }
     //This prints the gifts for Day 5
-    public static void Day5(){
+    private static void Day5(){
         System.out.println("five golden rings,");
         Day4();
     }
     //This prints the gifts for Day 6
-    public static void Day6(){
+    private static void Day6(){
         System.out.println("six geese a-laying");
         Day5();
     }
     //This prints the first day
-    public static void Verse1(){
+    private static void Verse1(){
+        System.out.println();
         System.out.println("On the 1st day of \"Xmas\", my true love sent to me");
+        Day1();
     }
     //This prints the second day
-    public static void Verse2(){
+    private static void Verse2(){
         System.out.println("On the 2nd day of \"Xmas\", my true love sent to me");
+        Day2();
     }
     //This prints the third day
-    public static void Verse3(){
+    private static void Verse3(){
         System.out.println("On the 3rd day of \"Xmas\", my true love sent to me");
+        Day3();
     }
     //This prints the fourth day
-    public static void Verse4(){
+    private static void Verse4(){
         System.out.println("On the 4th day of \"Xmas\", my true love sent to me");
+        Day4();
     }
     //This prints the fifth day
-    public static void Verse5(){
+    private static void Verse5(){
         System.out.println("On the 5th day of \"Xmas\", my true love sent to me");
+        Day5();
     }
     //This prints the sixth day
-    public static void Verse6(){
+    private static void Verse6(){
         System.out.println("On the 6th day of \"Xmas\", my true love sent to me");
+        Day6();
     }
 }
