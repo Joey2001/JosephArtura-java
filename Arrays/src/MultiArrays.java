ba9d735f83bf1219a789b577043fd9b612e9ae14
joey2001 <19arturajoseph@bprep.org>
2018-11-29 09:20:42 -0800

    array stuff

---
+++ Arrays/src/MultiArrays.java
@@ -0,0 +1,45 @@
+public class MultiArrays {
+    public static void main(String[] args) {
+        //exercise 1
+        double[] arrayDoub = new double[1028];
+        //exercise 2
+        int[][] multiArray = new int[5][4];
+        //exercise 3
+        String[][] studentSeating = new String[6][4];
+        //exercise 4
+        /*
+           0   1   2   3
+        0 ""  ""  ""  ""
+        1 ""  ""  ""  ""
+        2 ""  ""  ""  ""
+        3 ""  ""  ""  ""
+        4 ""  ""  ""  ""
+        5 ""  ""  ""  ""
+        */
+        //exercise 5
+        studentSeating[0][0] = "Kele";
+        studentSeating[0][1] = "Drew";
+        studentSeating[0][2] = "Daniel";
+        studentSeating[0][3] = "Gabe";
+        studentSeating[1][0] = "Jade";
+        studentSeating[1][1] = "Connor";
+        studentSeating[1][2] = "Allen";
+        studentSeating[1][3] = "Grace";
+        studentSeating[2][0] = "Frank";
+        studentSeating[2][1] = "Yimo";
+        studentSeating[2][2] = "Seante";
+        studentSeating[2][3] = "Billy";
+        studentSeating[3][0] = "Nick";
+        studentSeating[3][1] = "Matthew T.";
+        studentSeating[3][2] = "Joey";
+        studentSeating[3][3] = "Jackson";
+        studentSeating[4][0] = "Eric";
+        studentSeating[4][1] = "Hunter";
+        studentSeating[4][2] = "Gavin";
+        studentSeating[4][3] = "Nick";
+        studentSeating[5][0] = "Matthew V.";
+        studentSeating[5][1] = "Issac";
+        studentSeating[5][2] = "Bob";
+        studentSeating[5][3] = "Joe";
+    }
+}
