26024ef6415eb3ec6f10b3239fa9588f306f7285
Joseph <josephartura@gmail.com>
2018-12-18 20:17:02 -0800

    Update Calculator.java

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -16,7 +16,7 @@ public class Calculator {
         Scanner in = new Scanner(System.in);
         Scanner out = new Scanner(System.in);
         String looper = "yes";
-        while (looper.equalsIgnoreCase("yes")) {
+        while (looper.equalsIgnoreCase("yes") || looper.equalsIgnoreCase("Y")) {
             System.out.println("Enter any fraction to add, subtract, multiply or divide:");
             String input = in.nextLine() + " ";
             stringParse(input);
@@ -33,23 +33,34 @@ public class Calculator {
             if (string.charAt(i) != ' ' && string.charAt(i) != '_') {
                 word += string.charAt(i);
             } else {
-//                System.out.println(word);
                 numberHold[count] = word;
                 count++;
                 word = "";
             }
         }
-        String a = numberHold[numberHold.length /2];
-        String b = numberHold[0];
-        String c = numberHold[1];
-        numberHold[0] = a;
-        numberHold[1] = b;
-        numberHold[2] = c;
-        String[] num = Arrays.copyOfRange(numberHold, 1, numberHold.length);
-        String[] sign = Arrays.copyOfRange(numberHold, 0, 1);
-        String fraction = num[1] + "/" + num[3] + "/";
-        int wholeNum = Integer.parseInt(num[0]);
-        int wholeNum2 = Integer.parseInt(num[2]);
+        String fraction;
+        int wholeNum;
+        int wholeNum2;
+        String[] sign;
+        if(numberHold[4] != null) {
+            String a = numberHold[numberHold.length / 2];
+            String b = numberHold[0];
+            String c = numberHold[1];
+            numberHold[0] = a;
+            numberHold[1] = b;
+            numberHold[2] = c;
+            String[] num = Arrays.copyOfRange(numberHold, 1, numberHold.length);
+            sign = Arrays.copyOfRange(numberHold, 0, 1);
+            fraction = num[1] + "/" + num[3] + "/";
+            wholeNum = Integer.parseInt(num[0]);
+            wholeNum2 = Integer.parseInt(num[2]);
+        }else{
+            String[] shortNum = Arrays.copyOfRange(numberHold, 0, 3);
+            sign = Arrays.copyOfRange(shortNum, 1,2);
+            fraction = shortNum[0] + "/" + shortNum[2] + "/";
+            wholeNum = 0;
+            wholeNum2 = 0;
+        }
         String word2 = "";
         int count2 = 0;
         String[] fractionHold = new String[4];
@@ -57,7 +68,6 @@ public class Calculator {
             if (fraction.charAt(i) != '/') {
                 word2 += fraction.charAt(i);
             } else {
-//                System.out.println(word2);
                 fractionHold[count2] = word2;
                 count2++;
                 word2 = "";
@@ -69,10 +79,6 @@ public class Calculator {
         int D = Integer.parseInt(fractionHold[3]);
         A += (wholeNum * B);
         C += (wholeNum2 * D);
-//        System.out.println(Arrays.toString(numberHold));
-//        System.out.println(Arrays.toString(num));
-//        System.out.println(Arrays.toString(sign));
-//        System.out.println(Arrays.toString(fractionHold));
         System.out.print("The answer is: ");
         if(sign[0].equals("+")){
             System.out.println(add(A, B, C, D));
@@ -88,17 +94,19 @@ public class Calculator {
     }
     private static String add(final int firstNume, final int firstDenom, final int secondNume, final int secondDenom) {
         int commonFactor = 1;
-        int limit = 0;
-        int numerator = ((firstNume * secondDenom) + (secondNume * firstDenom));
+        int limit;
+        int numerator = ((firstNume * secondDenom) - (-1 * secondNume * firstDenom));
         int denominator = (secondDenom * firstDenom);
         if(numerator > denominator){
             limit = numerator;
         }else{
             limit = denominator;
         }
-        for(int i = 2; i < limit; i++){
-            if(numerator % i == 0 && denominator % i == 0 && commonFactor % i != 0){
-                commonFactor *= i;
+        if((numerator % denominator) != 0) {
+            for (int i = 2; i < limit; i++) {
+                if (numerator % i == 0 && denominator % i == 0 && commonFactor % i != 0) {
+                    commonFactor *= i;
+                }
             }
         }
         numerator /= commonFactor;
@@ -108,8 +116,13 @@ public class Calculator {
             numerator -= denominator;
             wholeNumCount++;
         }
-        if(wholeNumCount > 0) {
+        if(wholeNumCount > 0 && numerator % denominator != 0) {
             return wholeNumCount + "_" + numerator + "/" + denominator;
+        }else if(numerator % denominator == 0){
+            int wholeNum = numerator / denominator;
+            return (wholeNum + wholeNumCount) + "";
+        }else if(numerator == 0){
+            return "0";
         }else{
             return numerator + "/" + denominator;
         }
@@ -117,7 +130,7 @@ public class Calculator {
 
     private static String subtract(final int firstNume, final int firstDenom, final int secondNume, final int secondDenom) {
         int commonFactor = 1;
-        int limit = 0;
+        int limit;
         int numerator = ((firstNume * secondDenom) - (secondNume * firstDenom));
         int denominator = (secondDenom * firstDenom);
         if(numerator > denominator){
@@ -125,9 +138,11 @@ public class Calculator {
         }else{
             limit = denominator;
         }
-        for(int i = 2; i < limit; i++){
-            if(numerator % i == 0 && denominator % i == 0 && commonFactor % i != 0){
-                commonFactor *= i;
+        if((numerator % denominator) != 0) {
+            for (int i = 2; i < limit; i++) {
+                if (numerator % i == 0 && denominator % i == 0 && commonFactor % i != 0) {
+                    commonFactor *= i;
+                }
             }
         }
         numerator /= commonFactor;
@@ -137,8 +152,13 @@ public class Calculator {
             numerator -= denominator;
             wholeNumCount++;
         }
-        if(wholeNumCount > 0) {
+        if(wholeNumCount > 0 && numerator % denominator != 0) {
             return wholeNumCount + "_" + numerator + "/" + denominator;
+        }else if(numerator % denominator == 0){
+            int wholeNum = numerator / denominator;
+            return (wholeNum + wholeNumCount) + "";
+        }else if(numerator == 0){
+            return "0";
         }else{
             return numerator + "/" + denominator;
         }
@@ -146,7 +166,7 @@ public class Calculator {
 
     private static String multiply(final int firstNume, final int firstDenom, final int secondNume, final int secondDenom) {
         int commonFactor = 1;
-        int limit = 0;
+        int limit;
         int numerator = (firstNume * secondNume);
         int denominator = (firstDenom * secondDenom);
         if(numerator > denominator){
@@ -154,9 +174,11 @@ public class Calculator {
         }else{
             limit = denominator;
         }
-        for(int i = 2; i < limit; i++){
-            if(numerator % i == 0 && denominator % i == 0 && commonFactor % i != 0){
-                commonFactor *= i;
+        if((numerator % denominator) != 0) {
+            for (int i = 2; i < limit; i++) {
+                if (numerator % i == 0 && denominator % i == 0 && commonFactor % i != 0) {
+                    commonFactor *= i;
+                }
             }
         }
         numerator /= commonFactor;
@@ -166,8 +188,13 @@ public class Calculator {
             numerator -= denominator;
             wholeNumCount++;
         }
-        if(wholeNumCount > 0) {
+        if(wholeNumCount > 0 && numerator % denominator != 0) {
             return wholeNumCount + "_" + numerator + "/" + denominator;
+        }else if(numerator % denominator == 0){
+            int wholeNum = numerator / denominator;
+            return (wholeNum + wholeNumCount) + "";
+        }else if(numerator == 0){
+            return "0";
         }else{
             return numerator + "/" + denominator;
         }
@@ -175,7 +202,7 @@ public class Calculator {
 
     private static String divide(final int firstNume, final int firstDenom, final int secondNume, final int secondDenom) {
         int commonFactor = 1;
-        int limit = 0;
+        int limit;
         int numerator = (firstNume * secondDenom);
         int denominator = (firstDenom * secondNume);
         if(numerator > denominator){
@@ -183,9 +210,11 @@ public class Calculator {
         }else{
             limit = denominator;
         }
-        for(int i = 2; i < limit; i++){
-            if(numerator % i == 0 && denominator % i == 0 && commonFactor % i != 0){
-                commonFactor *= i;
+        if((numerator % denominator) != 0) {
+            for (int i = 2; i < limit; i++) {
+                if (numerator % i == 0 && denominator % i == 0 && commonFactor % i != 0) {
+                    commonFactor *= i;
+                }
             }
         }
         numerator /= commonFactor;
@@ -195,22 +224,15 @@ public class Calculator {
             numerator -= denominator;
             wholeNumCount++;
         }
-        if(wholeNumCount > 0) {
+        if(wholeNumCount > 0 && numerator % denominator != 0) {
             return wholeNumCount + "_" + numerator + "/" + denominator;
+        }else if(numerator % denominator == 0){
+            int wholeNum = numerator / denominator;
+            return (wholeNum + wholeNumCount) + "";
+        }else if(numerator == 0){
+            return "0";
         }else{
             return numerator + "/" + denominator;
         }
-    }
-
-    public static int parseWhole(final String fraction) {
-        return 0;
-    }
-
-    public static int parseNume(final String fraction) {
-        return 0;
-    }
-
-    public static int parseDenom(final String fraction) {
-        return 0;
     }
 }
