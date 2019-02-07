0f5f6c3914a45ecb529a3c872588bf3602f87b9f
joey2001 <19arturajoseph@bprep.org>
2019-02-07 08:55:46 -0800

    s

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -7,8 +7,7 @@
      * Fraction Calculator
      *****************************************************
      */
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Calculator {
     public static void main(String[] args) {
@@ -25,20 +24,28 @@ public class Calculator {
     }
     private static void stringParse(String string){
         String word = "";
+        String numerator;
+        String denominator;
         int count = 0;
-        String[] numberHold = new String[5];
+        ArrayList<String> numberHold = new ArrayList<String>();
         string.replace('_', ' ');
         for (int i = 0; i < string.length(); i++) {
             if (string.charAt(i) != ' ' && string.charAt(i) != '_') {
                 word += string.charAt(i);
             } else {
                 System.out.println(word);
-                numberHold[count] = word;
+                numberHold.add(word);
                 count++;
                 word = "";
             }
         }
-        System.out.println(Arrays.toString(numberHold));
+        for(int i = 0; i < numberHold.size(); i++){
+            if(numberHold.get(i).contains("/")){
+                numerator = numberHold.get(i).substring(0, numberHold.get(i).indexOf("/"));
+                denominator = numberHold.get(i).substring(numberHold.get(i).indexOf("/"));
+            }
+        }
+        System.out.println(numberHold);
     }
     public static String add(final int firstNume, final int firstDenom, final int secondNume, final int secondDenom) {
             return "";
