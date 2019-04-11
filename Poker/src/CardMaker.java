49346723d7db03bd373552dd76de62306dbbf0bf
Joseph <josephartura@gmail.com>
2019-04-10 21:08:12 -0700

    Update CardMaker.java
    This is better now

--- Poker/src/CardMaker.java
+++ Poker/src/CardMaker.java
@@ -3,29 +3,83 @@ class CardMaker{
     static String[][] constructCard(){
         String topBot = " ------- ";
         String E = "|       |";
-        String[] m1 = {"|   ♥   |", "|   ♣   |", "|   ♦   |", "|   ♠   |"};
-        String[] m2 = {"| ♥   ♥ |", "| ♣   ♣ |", "| ♦   ♦ |", "| ♠   ♠ |"};
-        String[] m3 = {"| ♥ ♥ ♥ |", "| ♣ ♣ ♣ |", "| ♦ ♦ ♦ |", "| ♠ ♠ ♠ |"};
-        String[] m4 = {"|♥ ♥ ♥ ♥|", "|♣ ♣ ♣ ♣|", "|♦ ♦ ♦ ♦|", "|♠ ♠ ♠ ♠|"};
 
-        String[] rL = {"| J     |", "| Q     |", "| K     |"};
-        String[] lL = {"|     J |", "|     Q |", "|     K |"};
+        String[] one   = {"|   ♥   |", "|   ♣   |", "|   ♦   |", "|   ♠   |"};
+        String[] two   = {"| ♥   ♥ |", "| ♣   ♣ |", "| ♦   ♦ |", "| ♠   ♠ |"};
+        String[] three = {"| ♥ ♥ ♥ |", "| ♣ ♣ ♣ |", "| ♦ ♦ ♦ |", "| ♠ ♠ ♠ |"};
+        String[] four  = {"|♥ ♥ ♥ ♥|", "|♣ ♣ ♣ ♣|", "|♦ ♦ ♦ ♦|", "|♠ ♠ ♠ ♠|"};
 
-        String[][] Ace = {{topBot, E, m1[0], E, topBot}, {topBot, E, m1[1], E, topBot}, {topBot, E, m1[2], E, topBot}, {topBot, E, m1[3], E, topBot}};
-        String[][] Two = {{topBot, m1[0], E, m1[0], topBot}, {topBot, m1[1], E, m1[1], topBot}, {topBot, m1[2], E, m1[2], topBot}, {topBot, m1[3], E, m1[3], topBot}};
-        String[][] Three = {{topBot, m1[0], m1[0], m1[0], topBot}, {topBot, m1[1], m1[1], m1[1], topBot}, {topBot, m1[2], m1[2], m1[2], topBot}, {topBot, m1[3], m1[3], m1[3], topBot}};
-        String[][] Four = {{topBot, m2[0], E, m2[0], topBot}, {topBot, m2[1], E, m2[1], topBot}, {topBot, m2[2], E, m2[2], topBot}, {topBot, m2[3], E, m2[3], topBot}};
-        String[][] Five = {{topBot, m2[0], m1[0], m2[0], topBot}, {topBot, m2[1], m1[1], m2[1], topBot}, {topBot, m2[2], m1[2], m2[2], topBot}, {topBot, m2[3], m1[3], m2[3], topBot}};
-        String[][] Six = {{topBot, m2[0], m2[0], m2[0], topBot}, {topBot, m2[1], m2[1], m2[1], topBot}, {topBot, m2[2], m2[2], m2[2], topBot}, {topBot, m2[3], m2[3], m2[3], topBot}};
-        String[][] Seven = {{topBot, m2[0], m3[0], m2[0], topBot}, {topBot, m2[1], m3[1], m2[1], topBot}, {topBot, m2[2], m3[2], m2[2], topBot}, {topBot, m2[3], m3[3], m2[3], topBot}};
-        String[][] Eight = {{topBot, m3[0], m2[0], m3[0], topBot}, {topBot, m3[1], m2[1], m3[1], topBot}, {topBot, m3[2], m2[2], m3[2], topBot}, {topBot, m3[3], m2[3], m3[3], topBot}};
-        String[][] Nine = {{topBot, m3[0], m3[0], m3[0], topBot}, {topBot, m3[1], m3[1], m3[1], topBot}, {topBot, m3[2], m3[2], m3[2], topBot}, {topBot, m3[3], m3[3], m3[3], topBot}};
-        String[][] Ten = {{topBot, m3[0], m4[0], m3[0], topBot}, {topBot, m3[1], m4[1], m3[1], topBot}, {topBot, m3[2], m4[2], m3[2], topBot}, {topBot, m3[3], m4[3], m3[3], topBot}};
-        String[][] Jack = {{topBot, rL[0], m1[0], lL[0], topBot}, {topBot, rL[0], m1[1], lL[0], topBot}, {topBot, rL[0], m1[2], lL[0], topBot}, {topBot, rL[0], m1[3], lL[0], topBot}};
-        String[][] Queen = {{topBot, rL[1], m1[0], lL[1], topBot}, {topBot, rL[1], m1[1], lL[1], topBot}, {topBot, rL[1], m1[2], lL[1], topBot}, {topBot, rL[1], m1[3], lL[1], topBot}};
-        String[][] King = {{topBot, rL[2], m1[0], lL[2], topBot}, {topBot, rL[2], m1[1], lL[2], topBot}, {topBot, rL[2], m1[2], lL[2], topBot}, {topBot, rL[2], m1[3], lL[2], topBot}};
+        String[] right = {"| J     |", "| Q     |", "| K     |"};
+        String[] left  = {"|     J |", "|     Q |", "|     K |"};
 
-        return new String[][]{Ace[0], Two[0], Three[0], Four[0], Five[0], Six[0], Seven[0], Eight[0], Nine[0], Ten[0], Jack[0], Queen[0], King[0], Ace[1], Two[1], Three[1], Four[1], Five[1], Six[1], Seven[1], Eight[1], Nine[1], Ten[1], Jack[1], Queen[1], King[1], Ace[2], Two[2], Three[2], Four[2], Five[2], Six[2], Seven[2], Eight[2], Nine[2], Ten[2], Jack[2], Queen[2], King[2], Ace[3], Two[3], Three[3], Four[3], Five[3], Six[3], Seven[3], Eight[3], Nine[3], Ten[3], Jack[3], Queen[3], King[3]};
+        String[][] parts = {{"|       |"}, one, two, three, four, right, left};
+        String[][] e = new String[52][5];
+
+        int notCount = 0;
+        int count = 0;
+        int falseLoop;
+        int JQK = 0;
+        int JQK2 = 0;
+        for(int i = 0; i < e.length; i++){
+            if(i % 13 == 0 && i != 0){
+                count++;
+                notCount = 0;
+            }
+            for(int j = 0; j < e[i].length; j++){
+                falseLoop = (i - (count * 13));
+
+                if(j == 0 || j == 4){
+                    e[i][j] = topBot;
+                }else if(j == 2){
+                    if((falseLoop % 2 == 0 && falseLoop != 5 && (falseLoop < 10)) || (falseLoop == 0 && i !=0)){
+                        notCount++;
+                        if(notCount == 0){
+                            e[i][j] = parts[notCount][0];
+                        }else{
+                            e[i][j] = parts[notCount][count];
+                        }
+                    }else if(falseLoop == 5 || falseLoop == 9){
+                        notCount++;
+                        e[i][j] = parts[notCount][count];
+                    }else if(falseLoop > 9){
+                        notCount = 1;
+                        e[i][j] = parts[1][count];
+                    }else{
+                        notCount--;
+                        if(notCount == 0){
+                            e[i][j] = parts[notCount][0];
+                        }else{
+                            e[i][j] = parts[notCount][count];
+                        }
+                    }
+                }else{
+                    if(falseLoop == 0){
+                        e[i][j] = E;
+                    }else if(falseLoop < 3){
+                        e[i][j] = parts[1][count];
+                    }else if(falseLoop < 7){
+                        e[i][j] = parts[2][count];
+                    }else if(falseLoop < 10){
+                        e[i][j] = parts[3][count];
+                    }else{
+                        if(j == 1) {
+                            e[i][j] = parts[5][JQK];
+                            JQK++;
+                            if (JQK == 3) {
+                                JQK = 0;
+                            }
+                        }else{
+                            e[i][j] = parts[6][JQK2];
+                            JQK2++;
+                            if (JQK2 == 3) {
+                                JQK2 = 0;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return e;
     }
 
     static void printCard(String[][] card){
